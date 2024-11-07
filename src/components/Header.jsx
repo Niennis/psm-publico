071c5ef1a543915890d921fbb1ff1f758cb460d0
Estefania <estefania.osses.v@gmail.com>
2024-11-07 11:36:57 -0300

    btn reservar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,8 +58,7 @@ const Header = () => {
   const pages = EVENTS !== 0 ? pagesWithEvents : pagesWithoutEvents
   const [style, setStyle] = useState({ width: 'min-content' });
 
-  const BOTON_RESERVAR = process.env.NEXT_PUBLIC_ACTIVATE_BUTTON === true
-  console.log('BOTON', BOTON_RESERVAR)
+  const BOTON_RESERVAR = process.env.NEXT_PUBLIC_ACTIVATE_BUTTON === "true"
 
   const isMediumSize = useMediaQuery("(min-width : 641px) and (max-width : 768px)");
   const isLargeSize = useMediaQuery("(min-width : 769px)");
